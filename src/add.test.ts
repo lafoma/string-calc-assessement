a2c7ae870b1037cb7746c53e4bc2912ae13113c4
lafoma <we@lafomacreative.agency>
2024-08-09 17:55:27 +0530

    added support for new lines

--- src/add.test.ts
+++ src/add.test.ts
@@ -5,6 +5,13 @@ describe('String Addition TDD', () => {
   test('should add nums delimited by comma', () => {
     expect(add('1,5')).toBe(6);
   });
+  test('should return 0 when a blank string is provided as input', () => {
+    expect(add('')).toBe(0);
+  })
+  test('should handle new lines between numbers. for e.g. add("1\n2,3") would output 6', () => {
+    expect(add('1\n2,3')).toBe(6);
+  })
+
 });
 
 
--- src/add.ts
+++ src/add.ts
@@ -1,6 +1,8 @@
 export const add = (str: string): any => {
   if (!str) return 0;
 
+  // handle new lines
+  str = str.split('\n').map(subStr => subStr.trim()).join(','); 
   const nums = str.split(',');
   return nums.reduce((acc, unit) => acc + Number(unit), 0);
 }
