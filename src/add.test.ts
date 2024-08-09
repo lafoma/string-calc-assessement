29e8a0870bdd1707ba3f50332c3b01d3f96469e7
lafoma <we@lafomacreative.agency>
2024-08-09 18:06:45 +0530

    now supports custom delimiters

--- src/add.test.ts
+++ src/add.test.ts
@@ -11,7 +11,7 @@ describe('String Addition TDD', () => {
   test('should handle new lines between numbers. for e.g. add("1\n2,3") would output 6', () => {
     expect(add('1\n2,3')).toBe(6);
   })
-  test('should handle different delimiters', () => {
+  test('should support different delimiters', () => {
     expect(add('//;\n1;2')).toBe(3);
   })
 
--- src/add.ts
+++ src/add.ts
@@ -1,9 +1,22 @@
 export const add = (str: string): any => {
+  const parseStr = (str: string): any => { 
+    const delimRegxp = /^\/\/(.)\n/;
+    const match = str.match(delimRegxp);
+    if (!match) throw new Error('Please check the expression.');
+    const delim = match[1];
+    const expr = str.slice(match[0].length);
+    return { delim, expr };
+  };
   if (!str) return 0;
-
-  // handle new lines
-  str = str.split('\n').map(subStr => subStr.trim()).join(','); 
-  const nums = str.split(',');
-  return nums.reduce((acc, unit) => acc + Number(unit), 0);
+  let nums;
+  if (str.startsWith('//')) {
+    const {delim, expr} = parseStr(str);
+    nums = expr.split(delim);
+  } else {
+    // handle new lines
+    str = str.split('\n').map(subStr => subStr.trim()).join(','); 
+    nums = str.split(',');
+  }
+  return nums.reduce((acc: number, unit: number) => acc + Number(unit), 0);
 }
 
